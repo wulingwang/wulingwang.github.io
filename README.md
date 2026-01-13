7ed3350b5d668c0402c66db3ad36dea9f70c25c5
wulingwang <zhangcg061212@outlook.com>
2026-01-13 11:11:39 +0800

    Site updated: 2026-01-13 11:11:39

---
+++ README.md
@@ -0,0 +1,1 @@
+https://wulingwang.github.io/
--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/337a.html","posts/0.html","posts/27eb.html","posts/0.html","posts/123c.html","posts/1e39.html"];function toRandomPost(){
+var posts=["posts/123c.html","posts/1e39.html","posts/337a.html","posts/27eb.html","posts/0.html","posts/0.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
